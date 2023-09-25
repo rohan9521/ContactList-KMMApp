94452555c921f25f99154f73db7b3ff91e277741
rohan9521 <rayrohan909@gmail.com>
2023-09-25 12:59:17 +0530

    Uncommented theme code and changed compileSDk to 34

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.contactlistapp.android"
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.example.contactlistapp.android"
         minSdk = 24
